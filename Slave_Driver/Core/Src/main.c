d1d807be2700e456b3068c4dfe2042a67612dcd2
Huynh Anh Duy <anhduyhuynh2002@gmail.com>
2023-09-15 14:55:06 +0700

    Check CANBus

--- Slave_Driver/Core/Src/main.c
+++ Slave_Driver/Core/Src/main.c
@@ -126,11 +126,13 @@ int main(void)
   {
 	  if(flag1){
 		  flag1 = 0;
+		  HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
 		  canctrl_Send(&hcan);
 		  HAL_Delay(1000);
 	  }
 	  if(flag2){
 		  flag2 = 0;
+		  HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
 		  canctrl_Send(&hcan);
 		  HAL_Delay(1000);
 	  }
@@ -224,12 +226,24 @@ static void MX_CAN_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LED_Pin */
+  GPIO_InitStruct.Pin = LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
