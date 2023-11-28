27ebcfdad62b532f1ace095178e55624eeb97300
TM-khoa <minhkhoa9a31314@gmail.com>
2023-11-28 15:52:45 +0700

    build MPU6050 lib.cpp with write and read method, test WHO_AM_I register OK, can not read temperature value

---
+++ Core/Inc/MPU6050.h
@@ -0,0 +1,48 @@
+/*
+ * MPU6050.h
+ *
+ *  Created on: Nov 28, 2023
+ *      Author: KHOA
+ */
+
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
+#include "main.h"
+#include "MPU6050_RegisterDef.h"
+#include "stdbool.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#define MPU6050_ADDRESS_AD0_LOW     0x68 // address pin low (GND), default for InvenSense evaluation board
+#define MPU6050_ADDRESS_AD0_HIGH    0x69 // address pin high (VCC)
+#define MPU6050_DEFAULT_ADDRESS     MPU6050_ADDRESS_AD0_LOW
+
+#define INIT_NOT_VALID 0
+#define INIT_VALID 1
+
+#ifdef __cplusplus
+
+class MPU6050 {
+	private:
+		I2C_HandleTypeDef *i2c;
+		uint8_t address;
+		uint8_t initValid;
+		public:
+		void Init(I2C_HandleTypeDef *i2c, bool addressHigh);
+		HAL_StatusTypeDef Write(MPU6050_RegisterAddress reg, uint8_t data);
+		uint8_t Read(MPU6050_RegisterAddress reg);
+		HAL_StatusTypeDef SendBurst();
+		HAL_StatusTypeDef ReceiveBurst();
+		HAL_StatusTypeDef IsReady();
+		uint16_t GetTemperature();
+};
+
+#endif
+
+#endif /* INC_MPU6050_H_ */
---
+++ Core/Src/MPU6050.cpp
@@ -0,0 +1,44 @@
+/*
+ * MPU6050.cpp
+ *
+ *  Created on: Nov 28, 2023
+ *      Author: KHOA
+ */
+
+#include "MPU6050.h"
+
+void MPU6050::Init(I2C_HandleTypeDef *i2c, bool addressHigh) {
+	this->i2c = i2c;
+	if (addressHigh)
+		this->address = MPU6050_ADDRESS_AD0_HIGH;
+	else
+		this->address = MPU6050_ADDRESS_AD0_LOW;
+	this->initValid = 1;
+}
+
+HAL_StatusTypeDef MPU6050::IsReady() {
+	if (!this->initValid) return HAL_ERROR;
+	return HAL_I2C_IsDeviceReady(this->i2c, this->address << 1, 2, HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef MPU6050::Write(MPU6050_RegisterAddress reg, uint8_t data) {
+	if (!this->initValid) return HAL_ERROR;
+	uint8_t dataTemp[1];
+	dataTemp[0] = data;
+	return HAL_I2C_Master_Transmit(this->i2c, this->address << 1, dataTemp, 1, HAL_MAX_DELAY);
+
+}
+
+uint8_t MPU6050::Read(MPU6050_RegisterAddress reg) {
+	while (!this->initValid);
+	uint8_t data[1];
+	data[0] = reg;
+	HAL_I2C_Master_Transmit(this->i2c, this->address << 1, data, 1, HAL_MAX_DELAY);
+	HAL_I2C_Master_Receive(this->i2c, this->address << 1, data, 1, HAL_MAX_DELAY);
+	return data[0];
+}
+
+uint16_t MPU6050::GetTemperature() {
+	uint16_t data = (this->Read(TEMP_OUT_H) << 8 | this->Read(TEMP_OUT_L));
+	return data;
+}
