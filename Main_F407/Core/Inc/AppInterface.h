6be89cd8731fd63f6909cdfc3431632d419df9e5
TM-khoa <minhkhoa9a31314@gmail.com>
2024-01-26 17:01:50 +0700

    Update: add command list, AppProtocol and ParamPID struct

--- Main_F407/Core/Inc/AppInterface.h
+++ Main_F407/Core/Inc/AppInterface.h
@@ -9,11 +9,46 @@
 #define INC_APPINTERFACE_H_
 #include "main.h"
 #include "string.h"
+
+#define COMMAND_LIST_LENGTH 1
+#define CRC16_LEN	2
+
 typedef union FloatByteConvert {
 	float f;
 	uint8_t b[4];
 } FloatByteConvert;
 
+typedef enum CommandList {
+	CMD_Start,
+	CMD_SetpointX,
+	CMD_SetpointY,
+	CMD_SetpointTheta,
+	CMD_TimeStableX,
+	CMD_TimeStableY,
+	CMD_TimeStableTheta,
+	CMD_OffsetX,
+	CMD_OffsetY,
+	CMD_OffsetTheta,
+	CMD_CurrentState,
+	CMD_MainPID,
+	CMD_End,
+} CommandList;
+
+typedef struct MainF407_ParamPID {
+	float kp;
+	float kd;
+	float alpha;
+	float limitHigh;
+	float limitLow;
+	float offsetSteady;
+} MainF407_ParamPID;
+
+typedef struct AppProtocol {
+	uint8_t dataLen;
+	uint16_t crc16;
+	CommandList cmdList;
+	uint8_t *data;
+} AppProtocol;
 void appinf_Init(UART_HandleTypeDef *huart);
 char* appinf_GetBufferAddress();
 void appintf_SendFloat(float num);
