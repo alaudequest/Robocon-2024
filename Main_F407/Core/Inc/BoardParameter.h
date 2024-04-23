c2317fb5d6086799b04d32a6a18f96150f1ef93b
Huynh Anh Duy <anhduyhuynh2002@gmail.com>
2024-04-23 17:54:08 +0700

    feat: add buzzer beep func

--- Main_F407/Core/Inc/BoardParameter.h
+++ Main_F407/Core/Inc/BoardParameter.h
@@ -2,19 +2,25 @@
  * BoardParameter.h
  *
  *  Created on: Sep 23, 2023
- *      Author: KHOA
+ *      Author: SpiritBoi
  */
 
 #ifndef INC_BOARDPARAMETER_H_
 #define INC_BOARDPARAMETER_H_
 #include "main.h"
 #include "PID.h"
+#include "stdbool.h"
 #include "InverseKinematic.h"
+
 
 typedef enum MainF4Robot1TypePID {
 	PID_RULO_1 = 1,
 	PID_RULO_2,
 	PID_ROBOT_THETA,
 } MainF4Robot1TypePID;
+
+void ProcessDelay(uint32_t delayMs, uint8_t *step);
+HAL_StatusTypeDef BuzzerBeep_Start(uint8_t repeatTime, uint32_t onDelayMs,uint32_t offDelayMs);
+void BuzzerBeepProcess();
 
 #endif /* INC_BOARDPARAMETER_H_ */
--- Main_F407/Core/Src/BoardParameter.c
+++ Main_F407/Core/Src/BoardParameter.c
@@ -2,29 +2,65 @@
  * BoardParameter.c
  *
  *  Created on: Sep 23, 2023
- *      Author: KHOA
+ *      Author: SpiritBoi
  */
 #include "BoardParameter.h"
 
+uint8_t buzzerBeepRepeatTime = 0;
+uint32_t buzzerOnDelayMs = 0;
+uint32_t buzzerOffDelayMs = 0;
+uint8_t buzzerStep = 0;
 
-//void brd_Init()
-//{
-//}
+void ProcessDelay(uint32_t delayMs, uint8_t *step)
+{
+	static bool isOnDelay = false;
+	static uint32_t DelayTick = 0;
+	if (isOnDelay != true) {
+		DelayTick = HAL_GetTick();
+		isOnDelay = true;
+	}
+	if (HAL_GetTick() - DelayTick >= delayMs && isOnDelay) {
+		isOnDelay = false;
+		*step = *step + 1;
+	}
+}
 
 
-//PID_Param brd_GetPID(PID_type type){
-//	switch (PID_type) {
-//		case PID_DC_SPEED:
-//
-//			break;
-//		case PID_DC_ANGLE:
-//
-//			break;
-//		case PID_BLDC_SPEED:
-//
-//			break;
-//	}
-//	return ;
-//}
+HAL_StatusTypeDef BuzzerBeep_Start(uint8_t repeatTime, uint32_t onDelayMs,uint32_t offDelayMs)
+{
+	if(buzzerStep != 0) return HAL_ERROR;
+	buzzerBeepRepeatTime = repeatTime - 1;
+	buzzerOnDelayMs = onDelayMs;
+	buzzerOffDelayMs = offDelayMs;
+	buzzerStep = 1;
+	return HAL_OK;
+}
 
+void BuzzerBeepProcess()
+{
+	switch(buzzerStep){
+	case 1:
+		HAL_GPIO_WritePin(Buzzer_GPIO_Port, Buzzer_Pin, 1);
+		buzzerStep++;
+		break;
+	case 2:
+		ProcessDelay(buzzerOnDelayMs,&buzzerStep);
+		break;
+	case 3:
+		HAL_GPIO_WritePin(Buzzer_GPIO_Port, Buzzer_Pin, 0);
+		buzzerStep++;
+		break;
+	case 4:
+		ProcessDelay(buzzerOffDelayMs,&buzzerStep);
+		break;
+	case 5:
+		if(buzzerBeepRepeatTime > 0) {
+			buzzerBeepRepeatTime --;
+			buzzerStep= 1;
+		}
+		else
+			buzzerStep= 0;
+		break;
+	}
+}
 
